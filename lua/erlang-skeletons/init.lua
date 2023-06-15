29642d8002f7da2e3d325c1c7b392b6e9021193b
Elias Assaf <elyas51000@gmail.com>
2023-06-15 12:15:46 +0300

    make LoadTemplate more general
    allow using a lua table with tpl_dir for the directory path,
    tpl_file for the template filename, replace_buffer to replace the
    current buffer

--- lua/erlang-skeletons/init.lua
+++ lua/erlang-skeletons/init.lua
@@ -21,7 +21,7 @@ if utils.isempty(vim.g.erl_replace_buffer) then
 end
 
 local M = {}
-M.LoadTemplate = function(tpl_file)
+M.LoadTemplate = function(tb)
 	local _T = {}
 	_T.fulldate = os.date()
 	_T.year = os.date("%Y")
@@ -31,8 +31,8 @@ M.LoadTemplate = function(tpl_file)
 	_T.name = vim.api.nvim_buf_get_name(0) or "untitled"
 	_T.basename = _T.name:match("/([%w%c%d]+)%.?[%w%c%d]*$") or _T.name
 
-	_T.tpl_dir = vim.g.erl_tpl_dir
-	_T.tpl = _T.tpl_dir .. tpl_file
+	_T.tpl_dir = tb.tpl_dir or vim.g.erl_tpl_dir
+	_T.tpl = _T.tpl_dir .. tb.tpl_file
 
 	local file = io.open(_T.tpl, "rb")
 	if not file then return nil end
@@ -45,7 +45,7 @@ M.LoadTemplate = function(tpl_file)
 		basename=_T.basename, year=_T.year, company=_T.company})
 
 
-	if vim.g.erl_replace_buffer == 1 then
+	if tb.replace_buffer or vim.g.erl_replace_buffer == 1 then
 		vim.api.nvim_buf_set_lines(0, 0, -1, false, {})
 	end
 
@@ -54,16 +54,16 @@ M.LoadTemplate = function(tpl_file)
 end
 
 M.setup = function()
-	vim.api.nvim_create_user_command("EQCStatem", function() M.LoadTemplate("eqc_statem") end, { desc = "Load EQCStatem template" })
-	vim.api.nvim_create_user_command("ErlServer", function() M.LoadTemplate("gen_server") end, { desc = "Load gen_server template" })
-	vim.api.nvim_create_user_command("ErlFsm", function() M.LoadTemplate("gen_fsm") end, { desc = "Load gen_fsm template" })
-	vim.api.nvim_create_user_command("ErlStatem", function() M.LoadTemplate("gen_statem") end, { desc = "Load gen_statem template" })
-	vim.api.nvim_create_user_command("ErlSupervisor", function() M.LoadTemplate("supervisor") end, { desc = "Load supervisor template" })
-	vim.api.nvim_create_user_command("ErlEvent", function() M.LoadTemplate("gen_event") end, { desc = "Load gen_event template" })
-	vim.api.nvim_create_user_command("ErlApplication", function() M.LoadTemplate("application") end, { desc = "Load application template" })
-	vim.api.nvim_create_user_command("ErlCTSuite", function() M.LoadTemplate("commontest") end, { desc = "Load commontest template" })
-	vim.api.nvim_create_user_command("ErlEscript", function() M.LoadTemplate("escript") end, { desc = "Load escript template" })
-	vim.api.nvim_create_user_command("ErlTemplate", function(tb) M.LoadTemplate(tb.tpl_file) end, { desc = "Load a custom template" })
+	vim.api.nvim_create_user_command("EQCStatem", function() M.LoadTemplate({tpl_file = "eqc_statem", }) end, { desc = "Load EQCStatem template" })
+	vim.api.nvim_create_user_command("ErlServer", function() M.LoadTemplate({tpl_file = "gen_server", }) end, { desc = "Load gen_server template" })
+	vim.api.nvim_create_user_command("ErlFsm", function() M.LoadTemplate({tpl_file = "gen_fsm", }) end, { desc = "Load gen_fsm template" })
+	vim.api.nvim_create_user_command("ErlStatem", function() M.LoadTemplate({tpl_file = "gen_statem", }) end, { desc = "Load gen_statem template" })
+	vim.api.nvim_create_user_command("ErlSupervisor", function() M.LoadTemplate({tpl_file = "supervisor", }) end, { desc = "Load supervisor template" })
+	vim.api.nvim_create_user_command("ErlEvent", function() M.LoadTemplate({tpl_file = "gen_event", }) end, { desc = "Load gen_event template" })
+	vim.api.nvim_create_user_command("ErlApplication", function() M.LoadTemplate({tpl_file = "application", }) end, { desc = "Load application template" })
+	vim.api.nvim_create_user_command("ErlCTSuite", function() M.LoadTemplate({tpl_file = "commontest", }) end, { desc = "Load commontest template" })
+	vim.api.nvim_create_user_command("ErlEscript", function() M.LoadTemplate({tpl_file = "escript", }) end, { desc = "Load escript template" })
+	vim.api.nvim_create_user_command("ErlTemplate", function(tb) M.LoadTemplate(tb) end, { desc = "Load a custom template" })
 end
 
 return M
